c647645107cd88c9f5c2c7e593316af7b34da0c3
raiyannawaz <raiyannawaz95@gmail.com>
2025-09-03 13:36:23 +0530

    Email redirect changes

--- src/components/custom/AuthCallback.jsx
+++ src/components/custom/AuthCallback.jsx
@@ -6,11 +6,15 @@ export default function AuthCallback() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    supabase.auth.getSession().then(({ data }) => {
-      if (data.session) {
-        navigate("/dashboard"); // or /sign-in
+    const process = async () => {
+      const { data, error } = await supabase.auth.getSession()
+      if (error) {
+        console.error("Session error:", error)
+        return
       }
-    });
+      navigate("/")
+    }
+    process()
   }, [navigate]);
 
   return <div className="h-svh w-svw bg-slate-50 flex justify-center items-center"><p>Completing sign in...</p></div>;
